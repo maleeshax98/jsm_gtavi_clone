ea0f76c9b5a50348569dc7f393b846a1543055e2
maleeshax98 <privatemaleesha@gmail.com>
2026-03-18 08:03:10 +0530

    Implement Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Hero from "./components/Hero";
+import Navbar from "./Navbar";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-
-
   return (
     <main>
-      <div>
-        <h1 className="text-3xl text-indigo-300">Welcome to the world of GTA and GSAP</h1>
-      </div>
+      <Navbar />
+      <Hero />
+     
     </main>
   );
 };
